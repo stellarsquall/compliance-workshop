eae594a84c96f08785817b0f1b95e901adec696a
Jody Wolfborn <jody.wolfborn (at) gmail.com>
2019-09-04 14:55:05 -0700

    adding Effortless Audit package

---
+++ report/habitat/plan.sh
@@ -0,0 +1,10 @@
+pkg_name=audit-baseline
+pkg_version=0.1.0
+pkg_origin=cheftv
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_description="Effortless Linux Audit Example"
+pkg_scaffolding="chef/scaffolding-chef-inspec"
+scaffold_automate_server_url='https://jwolf-a2.chef-demo.com'
+scaffold_automate_user='admin'
+scaffold_automate_token='VjlHjl44g30W_4t4buFsMs4ChFY='
