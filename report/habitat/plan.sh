64bdcb5621434b8fbbd3e5cb80d6c2da22b0f71a
stellarsquall <stellarsquall@protonmail.ch>
2019-09-16 22:40:42 -0500

    updated automate token and hostname for roadtrip Austin

--- report/habitat/plan.sh
+++ report/habitat/plan.sh
@@ -5,6 +5,6 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Effortless Linux Audit Example"
 pkg_scaffolding="chef/scaffolding-chef-inspec"
-scaffold_automate_server_url='https://roadtrip.workshops.learn.chef.io'
+scaffold_automate_server_url='https://roadtrip-austin.workshops.learn.chef.io'
 scaffold_automate_user='admin'
-scaffold_automate_token='_fRWgRmieD-9uO8Z5bXHbbt8fF0='
+scaffold_automate_token='vpRBp-ksr8nz8tgxIqRZ6XAhgtM='
